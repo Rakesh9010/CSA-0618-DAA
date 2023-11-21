e2265b70715f643bc4d8df52f8038445ce9c1ef0
Rakesh9010 <122259928+Rakesh9010@users.noreply.github.com>
2023-11-21 10:10:27 +0530

    Experiment13

---
+++ sum of digits.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+int digitSum(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+
+int maxSumWithEqualDigitSums(int nums[], int size) {
+    const int maxDigitSum = 200; 
+    int digitSumIndex[maxDigitSum * 2 + 1]; 
+    for (int i = 0; i < maxDigitSum * 2 + 1; i++) {
+        digitSumIndex[i] = -1; 
+    }
+
+    int maxSum = 0;
+    for (int i = 0; i < size; i++) {
+        int sum = digitSum(nums[i]);
+        if (digitSumIndex[sum] != -1) {
+            int index = digitSumIndex[sum];
+            maxSum = (nums[index] + nums[i] > maxSum) ? (nums[index] + nums[i]) : maxSum;
+        }
+        digitSumIndex[sum] = (digitSumIndex[sum] == -1) ? i : digitSumIndex[sum];
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int nums[size];
+
+    printf("Enter %d elements into the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int maxSum = maxSumWithEqualDigitSums(nums, size);
+    printf("Maximum value of nums[i] + nums[j]: %d\n", maxSum);
+
+    return 0;
+}
